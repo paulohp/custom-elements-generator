4a9b26371f2781d60f77fbd51df8b4779252c1fe
Paulo Pires <taralloqueiroiz@gmail.com>
2014-05-27 14:34:30 -0300

    added tray icin

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 var fs = require('fs');
+
+// Load native UI library
+var gui = require('nw.gui');
+
+// Create a tray icon
+var tray = new gui.Tray({icon: 'images/web-components_32x32.png' });
+
+// Give it a menu
+var menu = new gui.Menu();
+menu.append(new gui.MenuItem({ type: 'checkbox', label: 'box1' }));
+tray.menu = menu;
